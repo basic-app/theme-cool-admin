b03a64da64715069ebd47fb9fdeb9a6c0da7785c
basic-app <support@basic-app.com>
2019-02-05 18:45:07 +0300

    n/a

---
+++ PostButton.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Theme\CoolAdmin;
+
+use PHPTheme;
+use PHPTheme\Widget;
+
+class PostButton extends Widget
+{
+
+    public $tag = 'button';
+
+    public $options = [];
+
+    public $formOptions = [];
+
+    public $label;
+
+    public $url;
+
+    public function render()
+    {
+        $formOptions = $this->formOptions;
+
+        $formOptions['action'] = $this->url;
+
+        $formOptions['method'] => 'POST';
+
+        return PHPTheme::view('post-button', [
+            'tag' => $this->tag,
+            'options' => $this->options,
+            'label' => $this->label,
+            'url' => $this->url,
+            'formOptions' => $formOptions
+        ]);
+    }
+
+}
--- Views/table-button-delete.php
+++ Views/table-button-delete.php
@@ -1,26 +1,18 @@
 <?php
 
-
-/*
-    $content = admin_theme_view('_layouts/popup', [
-        'modalId' => $id,
-        'modalTitle' => t('admin', 'Delete'),
-        'modalBody' => '<p>' . t('admin', 'Are you sure?') . '</p>',
-        'modalFooterAction' => $url,
-        'modalFooterButton' => t('admin', 'Delete')
-    ]);
-
-    if (!array_key_exists('layoutBodyEnd', $this->data))
-    {
-        $this->data['layoutBodyEnd'] = '';
-    }
-
-    $this->data['layoutBodyEnd'] .= $content; 
-*/
-
 PHPTheme::$pageBodyEnd .= PHPTheme::widget('popup', [
     'id' => $id,
-    'title' => $label
+    'title' => $label,
+    'content' => '<p>' . PHPTheme::t('Are you sure?') . '</p>',
+    'footer' => PHPTheme::widget('PostButton', [
+        'tag' => 'button',
+        'options' => [
+            'class' => 'btn btn-primary'
+        ],
+        'label' => $label,
+        'url' => $url
+    ])
+
 ]);
 
 echo PHPTheme::beginTag($tag, $options);
