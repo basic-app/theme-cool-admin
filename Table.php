8a81395d8d14963f8c7e1975aa841f702f5ec576
basic-app <dev@basic-app.com>
2019-07-13 22:41:23 +0300

    n/a

--- Table.php
+++ Table.php
@@ -9,34 +9,4 @@ class Table extends \PhpTheme\CoolAdminTheme\Table
 
     const COLUMN = TableColumn::class;
 
-//    public $defaultPrimaryColumn = [];
-
-//    public $defaultTextColumn = [];
-
-//    public $defaultNumberColumn = [];
-
-    /*
-    public function primaryColumn(array $options = [])
-    {
-        $options = Html::mergeOptions($this->defaultPrimaryColumn, $options);
-
-        return $this->createColumn($options);
-    }
-
-    public function textColumn(array $options = [])
-    {
-        $options = Html::mergeOptions($this->defaultTextColumn, $options);
-
-        return $this->createColumn($options);
-    }
-
-    public function numberColumn(array $options = [])
-    {
-        $options = Html::mergeOptions($this->defaultNumberColumn, $options);
-
-        return $this->createColumn($options);
-    }
-    
-    */
-
 }
