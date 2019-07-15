d2a0844f420d2f77fd2297245bdee702a6c0484f
basic-app <dev@basic-app.com>
2019-07-15 13:19:58 +0300

    n/a

--- TableColumn.php
+++ TableColumn.php
@@ -14,74 +14,9 @@ class TableColumn extends \PhpTheme\CoolAdminTheme\TableColumn
         ]
     ];
 
-    public $displayExtraSmallOptions = ['class' => ['d-none', 'd-table-cell']];
-
-    public $displaySmallOptions = ['class' => ['d-none', 'd-sm-table-cell']];
-
-    public $displayMediumOptions = ['class' => ['d-none', 'd-md-table-cell']];
-
-    public $displayLargeOptions = ['class' => ['d-none', 'd-lg-table-cell']];
-
-    public $displayExtraLargeOptions = ['class' => ['d-none', 'd-xl-table-cell']];
-
     public function number()
     {
         $this->options = Html::mergeOptions($this->options, $this->numberOptions);
-
-        return $this;
-    }
-
-    public function displayExtraSmall()
-    {
-        $this->options = Html::mergeOptions($this->options, $this->displayExtraSmallOptions);
-
-        $this->headerOptions = Html::mergeOptions($this->headerOptions, $this->displayExtraSmallOptions);
-
-        $this->footerOptions = Html::mergeOptions($this->footerOptions, $this->displayExtraSmallOptions);
-
-        return $this;
-    }
-
-    public function displaySmall()
-    {
-        $this->options = Html::mergeOptions($this->options, $this->displaySmallOptions);
-
-        $this->headerOptions = Html::mergeOptions($this->headerOptions, $this->displaySmallOptions);
-
-        $this->footerOptions = Html::mergeOptions($this->footerOptions, $this->displaySmallOptions);
-
-        return $this;
-    }
-
-    public function displayMedium()
-    {
-        $this->options = Html::mergeOptions($this->options, $this->displayMediumOptions);
-
-        $this->headerOptions = Html::mergeOptions($this->headerOptions, $this->displayMediumOptions);
-
-        $this->footerOptions = Html::mergeOptions($this->footerOptions, $this->displayMediumOptions);
-
-        return $this;
-    }
-
-    public function displayLarge()
-    {
-        $this->options = Html::mergeOptions($this->options, $this->displayLargeOptions);
-
-        $this->headerOptions = Html::mergeOptions($this->headerOptions, $this->displayLargeOptions);
-
-        $this->footerOptions = Html::mergeOptions($this->footerOptions, $this->displayLargeOptions);
-
-        return $this;
-    }    
-
-    public function displayExtraLarge()
-    {
-        $this->options = Html::mergeOptions($this->options, $this->displayExtraLargeOptions);
-
-        $this->headerOptions = Html::mergeOptions($this->headerOptions, $this->displayExtraLargeOptions);
-
-        $this->footerOptions = Html::mergeOptions($this->footerOptions, $this->displayExtraLargeOptions);
 
         return $this;
     }
--- Theme.php
+++ Theme.php
@@ -11,6 +11,10 @@ class Theme extends \PhpTheme\CoolAdminTheme\Theme
 
     const TABLE = Table::class;
 
+    const TABLE_UPDATE_BUTTON_COLUMN = TableUpdateButtonColumn::class;
+
+    const TABLE_DELETE_BUTTON_COLUMN = TableDeleteButtonColumn::class;
+
     public $defaultFormOptions = [];
 
     public function createForm(array $options = [])
@@ -18,6 +22,13 @@ class Theme extends \PhpTheme\CoolAdminTheme\Theme
         $options = Html::mergeOptions($this->defaultFormOptions, $options);
 
         return $this->createWidget(static::FORM, $options);
+    }
+
+    public function updateButtonColumn(array $options = [])
+    {
+        $options = Html::mergeOptions($this->defaultUpdateButtonColumn, $options);
+
+        return $this->createWidget(static::TABLE_UPDATE_BUTTON_COLUMN, $options);
     }
     
 }
