6af33c34697825152c0266ebd9690a9c87d5e600
basic-app <dev@basic-app.com>
2019-10-12 00:03:18 +0300

    n/a

--- Config/Events.php
+++ Config/Events.php
@@ -1,25 +1,34 @@
 <?php
 
 use BasicApp\Helpers\CliHelper;
+use BasicApp\System\SystemEvents;
+use BasicApp\Admin\AdminEvents;
 
-CodeIgniter\Events\Events::on('install', function() {
+SystemEvents::onUpdate(function() {
 
     $themeDir = FCPATH . 'themes' . DIRECTORY_SEPARATOR . 'colorlib-cool-admin';
 
     CliHelper::downloadToFile('https://codeload.github.com/puikinsh/CoolAdmin/zip/master', $themeDir . '.zip');
+    
     CliHelper::zipExtractTo($themeDir . '.zip', $themeDir);
+    
     CliHelper::delete($themeDir . '.zip');
+    
     CliHelper::copy($themeDir . DIRECTORY_SEPARATOR . 'CoolAdmin-master/css', $themeDir . DIRECTORY_SEPARATOR . 'css');
+    
     CliHelper::copy($themeDir . DIRECTORY_SEPARATOR . 'CoolAdmin-master/fonts', $themeDir . DIRECTORY_SEPARATOR . 'fonts');
+    
     CliHelper::copy($themeDir . DIRECTORY_SEPARATOR . 'CoolAdmin-master/images', $themeDir . DIRECTORY_SEPARATOR . 'images');
+    
     CliHelper::copy($themeDir . DIRECTORY_SEPARATOR . 'CoolAdmin-master/js', $themeDir . DIRECTORY_SEPARATOR . 'js');
+    
     CliHelper::copy($themeDir . DIRECTORY_SEPARATOR . 'CoolAdmin-master/vendor', $themeDir . DIRECTORY_SEPARATOR . 'vendor');
+    
     CliHelper::delete($themeDir . DIRECTORY_SEPARATOR . 'CoolAdmin-master');
+    
     CliHelper::copy(dirname(__DIR__) . '/custom.css', $themeDir . DIRECTORY_SEPARATOR . 'custom.css');
 });
 
-CodeIgniter\Events\Events::on('admin_theme_list', function($event) {
-
+AdminEvents::onAdminThemeList(function($event) {
     $event->result['BasicApp\CoolAdminTheme\Theme'] = 'Cool Admin by Colorlib';
-
 });
